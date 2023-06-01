b315c3fc9ea8f8ed7847e8bab96287777656f9ff
LAPTOP-01MR4FIS\halim <halimovadalera@mail.ru>
2023-06-01 17:05:13 +0500

    PCG random

---
+++ PCG/Program.cs
@@ -0,0 +1,40 @@
+﻿using System.Diagnostics;
+
+var random = new Random();
+var pcg = new PCG.PCG();
+//var file = new StreamWriter(@"C:\Users\halim\RiderProjects\PCG\PCGTests\Sample.csv", false, Encoding.Default);
+
+Console.WriteLine("Hello, World!");
+Console.WriteLine("Экперимент на среднее время при 1_000 повторений, которые вызывают ф-ию рандома 100_000_000 раз.");
+Console.Write("C# realisation: ");
+Console.WriteLine(TimeExperiment(random, 1_000));
+Console.Write("PCG realisation: ");
+Console.WriteLine(TimeExperiment(pcg, 1_000));
+
+//file.Close();
+//Console.WriteLine($"{Convert.ToString(125, toBase: 10)}");
+
+long TimeExperiment(Random testRandom, int experimentCount)
+{
+    var summaryTime = 0L;
+    for (var i = 0; i < experimentCount; i++)
+        summaryTime += TestTimeRandom(testRandom);
+    return summaryTime / experimentCount;
+}
+
+long TestTimeRandom(Random testRandom)
+{
+    var timer = new Stopwatch();
+    timer.Start();
+    
+    MainRandom(testRandom);
+    
+    timer.Stop();
+    return timer.ElapsedMilliseconds;
+}
+
+void MainRandom(Random testRandom)
+{
+    for (var i = 0; i < 10_000_000; i++)
+        testRandom.Next();
+}
---
+++ PCGLibrary/Class1.cs
@@ -0,0 +1,92 @@
+﻿namespace PCGLibrary;
+
+public class PCG : Random
+{
+    private ulong _seed;
+    public ulong Seed
+    {
+        get => _seed; 
+        set => Initialization(value);
+    }
+    public ulong State { get; set; }
+
+    private const byte HighOrderBitsCount = 5;
+    private const byte AmountOfRotationUpToHighOrderBitsCountForInt64 = 64 - HighOrderBitsCount;
+    private const byte AmountOfRotationUpToHighOrderBitsCountForInt32 = 32 - HighOrderBitsCount;
+    private const byte AmountOfRotationForXorShift = (64 - AmountOfRotationUpToHighOrderBitsCountForInt32) / 2;
+    
+    private const ulong Multiplier = 6364136223846793005ul;
+    private const ulong Increment = 1442695040888963407ul | 1;
+    private const double DoubleConvertMultiplier = 1.0d / Multiplier;
+
+    public PCG() : this((ulong)Environment.TickCount64) { }
+    
+    public PCG(ulong seed) => Initialization(seed);
+
+    public override int Next() => (int)(NextUInt() >>> 1);
+
+    public override int Next(int maxValue) => (int)NextUInt((uint)maxValue);
+    
+    public override int Next(int minValue, int maxValue) => (int)NextUInt((uint)minValue, (uint)maxValue);
+
+    public uint NextUInt()
+    {
+        var oldState = State;
+        State = LCG(State);
+        
+        var xorShift = (uint)(XorShift(oldState) >>> AmountOfRotationUpToHighOrderBitsCountForInt32);
+        var rotationCount = (byte)(oldState >>> AmountOfRotationUpToHighOrderBitsCountForInt64);
+        return RandomRotation(xorShift, rotationCount);
+    }
+    
+    public uint NextUInt(uint maxValue)
+    {
+        var randomNumber = NextUInt();
+        var result = (ulong)maxValue * randomNumber;
+        result >>= 32;
+        return (uint)result;
+    }
+    
+    public uint NextUInt(uint minValue, uint maxValue)
+    {
+        if(minValue >= maxValue)
+            throw new ArgumentException("MaxValue must be larger than MinValue");
+
+        var offsetMaxValue = maxValue - minValue;
+        var result = NextUInt(offsetMaxValue);
+        return result + minValue;
+    }
+
+    public uint FastNextUInt()
+    {
+        var oldState = State;
+        State *= Multiplier;
+
+        oldState ^= oldState >>> 22;
+        var rotationCount = (byte)(oldState >>> 61);
+        return (uint)(oldState >> (22 + rotationCount));
+    }
+
+    public byte NextByte() => (byte)(NextUInt() >>> 24);
+
+    public bool NextBool() => NextUInt() % 2 == 1;
+
+    public override float NextSingle() => (float)Sample();
+    
+    public override double NextDouble() => Sample();
+
+    protected override double Sample() => NextUInt() * DoubleConvertMultiplier;
+
+    private static ulong LCG(ulong x) => Multiplier * x + Increment;
+
+    private static ulong XorShift(ulong x) => x ^ x >>> AmountOfRotationForXorShift;
+    
+    private static uint RandomRotation(uint x, byte shift) => x >>> shift | x << (-shift & 31);
+
+    private void Initialization(ulong seed)
+    {
+        _seed = seed;
+        State = Seed + Increment;
+        Next();
+    }
+}
